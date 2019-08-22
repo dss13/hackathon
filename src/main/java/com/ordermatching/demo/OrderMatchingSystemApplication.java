a26d20540a0c216b9497ecd5349dd75b1cda4437
dss13 <s.sumugan007@gmail.com>
2019-08-22 12:43:50 +0530

    initial commit

---
+++ src/main/java/com/ordermatching/demo/OrderMatchingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ordermatching.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderMatchingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderMatchingSystemApplication.class, args);
+	}
+
+}
